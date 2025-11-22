a9e10baaae4830a176adaed5efceda5a6fe91797
Mᴇᴇʀᴀ S <243485073+Meera-S-2006@users.noreply.github.com>
2025-11-22 13:09:32 +0530

    Update Images.py

--- api/Images.py
+++ api/Images.py
@@ -1,6 +1,12 @@
 
 IMAGES_URL = [
-  "", 
-  "" 
+  "https://envs.sh/-zJ.jpeg", 
+  "https://envs.sh/-zs.jpeg", 
+"https://envs.sh/-zN.jpeg", 
+  "https://envs.sh/-zH.jpeg" , 
+"https://envs.sh/-zg.jpeg", 
+
+
+  
 ] 
   
